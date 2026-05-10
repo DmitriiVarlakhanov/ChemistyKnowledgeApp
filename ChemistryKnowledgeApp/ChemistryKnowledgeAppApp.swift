5f2783b6959449468ebf9376ba6f7b0a22de1911
Dmitrii Varlakhanov <dmitriivarlakhanov@gmail.com>
2026-05-10 18:41:54 +0300

    Initial Commit

---
+++ ChemistryKnowledgeApp/ChemistryKnowledgeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemistryKnowledgeAppApp.swift
+//  ChemistryKnowledgeApp
+//
+//  Created by Dmitrii Varlakhanov on 5/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ChemistryKnowledgeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChemistryKnowledgeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChemistryKnowledgeApp
+//
+//  Created by Dmitrii Varlakhanov on 5/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
